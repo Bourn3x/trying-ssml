00767f8ba6950a3e986ddbc63d3c26379c442850
Julian Chong <julianchong10@gmail.com>
2024-02-06 23:32:27 +0800

    Use useRef instead of useState for tts input

--- src/pages/tts.tsx
+++ src/pages/tts.tsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import { useRef } from "react";
 import Spinner from "@/components/Spinner";
 import Button from "@/components/Button";
 import useSpeechEngine from "@/hooks/useSpeechEngine";
 
 export default function Home() {
   const { isLoading, isPlaying, handlePlay, handlePause } = useSpeechEngine();
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
-  const [inputText, setInputText] =
-    useState(`My name is Julian! I am pleased to meet you.
-
-Please feel free to type in any sentence.`);
+  const handleClickPlay = () => {
+    const inputValue = inputRef.current?.value;
+    if (!inputValue) return;
+    handlePlay(inputValue);
+  };
 
   return (
     <>
       <Button
         className="mb-4"
-        disabled={inputText.length === 0 || isLoading}
-        onClick={isPlaying ? handlePause : () => handlePlay(inputText)}
+        disabled={isLoading}
+        onClick={isPlaying ? handlePause : handleClickPlay}
       >
         {isLoading ? <Spinner /> : isPlaying ? "Pause" : "Play"}
       </Button>
 
       <textarea
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
+        ref={inputRef}
+        defaultValue={`My name is Julian! I am pleased to meet you.
+
+Please feel free to type in any sentence.`}
         style={{ height: "300px" }}
         className="w-full max-w-3xl rounded-md p-4 border-2 focus:outline-none focus:border-blue-200"
         placeholder="Type something!"
